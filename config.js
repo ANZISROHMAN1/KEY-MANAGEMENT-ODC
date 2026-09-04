008d55fa91af9be5c15e4dd26a6e83139a58b7e6
MacBook <macbook@MacBook-Air-MacBook.local>
2026-09-04 11:20:53 +0700

    chore: Update API_URL in config.js to new deployment

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 // Masukkan URL Google Apps Script Web App Anda di sini
-const API_URL = 'https://script.google.com/macros/s/AKfycbwVJOnyrmU6tOHgD7orFUevHZB9TjxL_bsJYNAhckp6tnuczAkNmxGi9SRGATi93yg2/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbztXFt8WmL1SYxQWduB9cmjXCC-rMEVz4V2x9ubAFMzPwsKj4soGpBHJCyQailp5c0D/exec';
